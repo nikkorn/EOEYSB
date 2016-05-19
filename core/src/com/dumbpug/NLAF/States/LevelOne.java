ffb58505e761d916964ed252b85b3abd23b5caae
Nikolas Howard (student) <nh163@moco207L-a6.university.brighton.ac.uk>
2016-05-19 20:28:31 +0100

    Rubbish commit to trigger Travis CI build

--- core/src/com/dumbpug/NLAF/States/LevelOne.java
+++ core/src/com/dumbpug/NLAF/States/LevelOne.java
@@ -5,20 +5,15 @@ public class LevelOne extends State {
 	@Override
 	public void stateLoad() {
 		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void renderState() {
 		// TODO Auto-generated method stub
-	
 	}
 
 	@Override
 	public void renderStateLoading() {
 		// TODO Auto-generated method stub
-		
 	}
-
-
 }
