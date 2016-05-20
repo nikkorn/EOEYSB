6af444db8bdcd4d65980ea4013ce99d4aa041d83
nikkorn <nikhoward1234@hotmail.co.uk>
2016-05-20 21:18:24 +0100

    Cleaned up FPSManager

--- core/src/com/dumbpug/NLAF/FPSManagement/FPSManager.java
+++ core/src/com/dumbpug/NLAF/FPSManagement/FPSManager.java
@@ -1,9 +1,11 @@
 package com.dumbpug.NLAF.FPSManagement;
 
+/**
+ * Manages the FPS of our game, compliments LibGDX approach.
+ */
 public class FPSManager {
 	private float time = 0;
 	private float DELAY;
-	
 	long lastFPS = System.nanoTime();
     int frameCount = 0;
     boolean showFPS = false;
@@ -12,27 +14,37 @@ public class FPSManager {
 	
 	public FPSManager(Updateable ref){
 		renderableObject = ref;
+        // Set a default FPS of 30.
 		DELAY = 1/30f;
 		lastFPS = System.nanoTime();
 	}
 	
     public FPSManager(Updateable ref, float delay){
     	renderableObject = ref;
+        // Set delay
 		DELAY = delay;
+        // Set last FPS
 		lastFPS = System.nanoTime();
 	}
-    
-    public void update(float dt){
-    	time += dt;
+
+    /**
+	 * Do update
+	 * @param deltaTimeVal
+     */
+    public void update(float deltaTimeVal){
+    	time += deltaTimeVal;
     	if(time >= DELAY){
     		renderableObject.controlledRender();
     		time -= DELAY;
     	}
+        // Are we actually displaying the FPS?
     	if(showFPS){
     		frameCount++;
     		if (System.nanoTime() > (lastFPS + 1000000000)) {
+				// Print our FPS
                 System.out.println("FPS : " + frameCount);
                 lastFPS = System.nanoTime();
+				// Reset frame count
                 frameCount = 0;
             }
     	}
