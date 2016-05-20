5e977e4571c16e244a51ce544475bab6b36a04aa
nikkorn <nikhoward1234@hotmail.co.uk>
2016-05-20 15:58:02 +0100

    Added first unit test

---
+++ core/test/GameMathUT.java
@@ -0,0 +1,16 @@
+import com.dumbpug.NLAF.Math.GameMath;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by nik on 20/05/16.
+ */
+public class GameMathUT {
+
+    @Test
+    public void checkCirclesIntersect() {
+       // GameMath.circlesIntersect();
+    }
+
+}
